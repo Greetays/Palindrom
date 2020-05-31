7f42a088c6c1902488810d8f51d2b351359806a8
Greetays <renat@DESKTOP-LEJB10D.localdomain>
2020-05-31 20:01:21 +0300

    Read text.txt

--- src/main.c
+++ src/main.c
@@ -3,15 +3,17 @@
 #include <locale.h>
 #define STR 100
 
-int main()
+int main(int argc, char *argv[])
 {
     char text[STR]; //создаём массив символов
     setlocale (LC_ALL, "Rus"); //подключаем русский язык
     FILE *text_file; //принимаем указатель на файл
-    text_file = fopen ("C:\\Users\\Greetrays Kap\\Desktop\\Учёба\\2 Семестр\\Курсовая\\Palindrom\\bin\\text1.txt", "r"); //открываем файл для чтения
-    while (fgets (text, STR, text_file)){
-      fprintf(stdout, "%s\n", text);
+    text_file = fopen (argv[1], "r"); //открываем файл для чтения
+    if(text_file != NULL){ //идёт проверка на NULL
+      while (fgets (text, STR, text_file)){ // получаем данные из файла
+        fprintf(stdout, "%s\n", text); //выводим данные в командную строку
+      }
     }
-    fclose(text_file);//закрываем файл
+  //  fclose(argv[1]);//закрываем файл
 return 0;
 }

38ab61775b690eab5a4759f9f55da73fae88eedd
Greetays <renat@DESKTOP-LEJB10D.localdomain>
2021-05-05 16:20:07 +0300

    Delete files

--- Makefile
+++
@@ -1,5 +0,0 @@
-all: palindrom
-palindrom: src/main.c src/processing_text.c src/search_pal.c
-		gcc -Wall src/main.c src/processing_text.c src/search_pal.c -o palindrom
-		./palindrom src/text.txt
-
--- src/palindrom.h
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-void search_pal(char *text1, char *text2, int size);
-void del_symbol(char *text, int count);
-void breaking_up (char *text, int size);
-int comstr(char* text1, char* text2, int size);
-void revers(char* text, int j);
